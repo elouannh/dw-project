2e9e126e0ca07e08a9251ef03adce5989b61b909
Mysokapg <poupeney.gi@gmail.com>
2024-02-09 16:06:57 +0100

    hfofao

--- src/script.js
+++ src/script.js
@@ -1,11 +1,32 @@
 // Script here
-const boomCursor = document.getElementsByClassName('boom-cursor').item();
-console.log(boomCursor);
+const boomCursor = document.getElementsByClassName('boom-cursor').item(0);
+let splitMargin = 1;
 
 document.body.addEventListener('mousedown', (event) => {
-    boomCursor.style.display = "block";
-    boomCursor.style.left = event.clientX;
-    boomCursor.style.top = event.clientY;
+    var nouvelleImage = document.createElement('img');
 
-    setTimeout(() => boomCursor.style.display = "none", 1300);
+    nouvelleImage.src = './img/explo.gif';
+
+    nouvelleImage.style.position = 'fixed';
+    nouvelleImage.style.width = '40px';
+    nouvelleImage.style.height = '40px';
+    nouvelleImage.style.left = (event.clientX - 8) + 'px';
+    nouvelleImage.style.top = (event.clientY - 8) + 'px';
+
+    document.body.appendChild(nouvelleImage);
+
+    setTimeout(function() { document.body.removeChild(nouvelleImage); }, 1000);
+});
+
+function splitCouple() {
+    splitMargin += splitMargin;
+
+    if (splitMargin - 500 > window.innerWidth) {
+        document.getElementById('split').style.display = "none";
+    }
+    else document.getElementById('split').style.marginLeft = `${splitMargin}px`;
+}
+
+document.getElementById('split').addEventListener('mousedown', (event) => {
+    splitCouple();
 });
