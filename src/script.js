29124c6249b892c4c13fe2ce01106c0c0c7be6ee
Mysokapg <poupeney.gi@gmail.com>
2024-02-09 16:06:15 +0100

    travail quasi fini

--- src/script.js
+++ src/script.js
@@ -1,1 +1,11 @@
 // Script here
+const boomCursor = document.getElementsByClassName('boom-cursor').item();
+console.log(boomCursor);
+
+document.body.addEventListener('mousedown', (event) => {
+    boomCursor.style.display = "block";
+    boomCursor.style.left = event.clientX;
+    boomCursor.style.top = event.clientY;
+
+    setTimeout(() => boomCursor.style.display = "none", 1300);
+});
